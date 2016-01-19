44b080609d932700d9a949c200605b9c56ed7dfc
Patrick Shushereba <pshushereba@gmail.com>
2016-01-19 16:30:02 +0000

    Working on image upload feature.

--- app/models/place.rb
+++ app/models/place.rb
@@ -1,9 +1,12 @@
 class Place < ActiveRecord::Base
 	belongs_to :user
 	has_many :comments
+	has_many :photos
 	
 	geocoded_by :address
 	after_validation :geocode
 	
-	validates :name, presence: true
+	validates :name, presence: true, :length => {:minimum => 3}
+	validates :address, presence: true
+	validates :description, presence: true
 end
